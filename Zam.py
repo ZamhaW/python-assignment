91c071f7db8d478db909cb196d4da8a8bcc12b05
ZamhaW <zwizluv@gmail.com>
2025-08-24 14:52:17 +0300

    Python commitment

---
+++ Zam.py
@@ -0,0 +1,20 @@
+def calculate_discount(price, discount_percent):
+    discount = price * discount_percent
+
+price = (input("Enter the price"))
+discount_percent = (input("Enter the discount_percent"))
+
+#checking conditions and apply discount
+if price:
+   discount_percent=0.02
+   print(input("Enter the final_price after applying discount"))
+
+
+
+ 
+ 
+
+
+
+
+    
